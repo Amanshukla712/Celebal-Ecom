5cf2e16a4bd406f52ca85ba7711f8f27c290d2bf
Amanshukla712 <120775204+Amanshukla712@users.noreply.github.com>
2024-07-06 21:26:23 +0530

    Add files via upload

---
+++ src/utils/stripe/stripe.utils.js
@@ -0,0 +1,3 @@
+import {loadStripe} from '@stripe/stripe-js';
+
+export const stripePromise = loadStripe("pk_test_51PYt1PLHumM0XD9k1xYQycI3aVh5oLeMi8WakCyovBhL3eKlucG6UyD5MFFSZ3PBreNsVraTD9V7PWxbvNSOWNOE00MWye4wO8");
